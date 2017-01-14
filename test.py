ab023e0fdd214225ad59d0aa0f8afe5b64ed0976
Ulli Hochholdinger <ulrich.hochholdinger@netpioneer.de>
2017-01-14 16:54:22 +0100

    Abstand

--- test.py
+++ test.py
@@ -4,10 +4,27 @@ from time import sleep
 import ev3dev.ev3 as ev3
 import threading
 
-ev3.Sound.speak('Welcome to the E V 3 dev project!').wait()
+class Head:
+    def __init__(self):
+        self.__distance = None
+        self.__us_sensor = ev3.UltrasonicSensor()
+        self.thread = threading.Thread(target=self.__read_sensor())
+        self.thread.start()
+        self.thread = threading.Thread(target=self.__action())
+        self.thread.start()
 
-# us_sensor = ev3.UltrasonicSensor()
-#
-# while True:
-#     print(us_sensor.distance_centimeters)
-#     sleep(0.1)
+    def __read_sensor(self):
+        while True:
+            self.__distance = self.__us_sensor.distance_centimeters()
+            sleep(0.001)
+
+    def __action(self):
+        while True:
+            if self.__distance <= 100:
+                print(self.__distance)
+            sleep(0.001)
+
+
+head = Head()
+sleep(100)
+
