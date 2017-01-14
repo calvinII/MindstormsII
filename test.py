4fe537e48734bd0700601124d24dba2357f75f8f
Ulli Hochholdinger <ulrich.hochholdinger@netpioneer.de>
2017-01-14 16:32:12 +0100

    Abstand

--- test.py
+++ test.py
@@ -2,9 +2,12 @@
 
 from time import sleep
 import ev3dev.ev3 as ev3
+import threading
 
-us_sensor = ev3.UltrasonicSensor()
+ev3.Sound.speak('Welcome to the E V 3 dev project!').wait()
 
-while True:
-    print(us_sensor.distance_centimeters)
-    sleep(0.1)
+# us_sensor = ev3.UltrasonicSensor()
+#
+# while True:
+#     print(us_sensor.distance_centimeters)
+#     sleep(0.1)
