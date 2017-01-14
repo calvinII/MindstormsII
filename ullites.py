e4d9d8e695564b454162e3317b25b5e8d287ccba
Ulli Hochholdinger <ulrich.hochholdinger@netpioneer.de>
2017-01-14 22:39:57 +0100

    AAAAAAAh!

--- ullites.py
+++ ullites.py
@@ -1,24 +1,8 @@
-import threading
 from time import sleep
+import ev3dev.ev3 as ev3
 
+us_sensor = ev3.UltrasonicSensor()
 
-def print_a():
-    while True:
-        print("a")
-        sleep(1)
-
-
-def print_b():
-    while True:
-        print("b")
-        sleep(0.5)
-
-a = threading.Thread(target=print_a())
-b = threading.Thread(target=print_b())
-a.setDaemon(True)
-b.setDaemon(True)
-a.start()
-print("a started")
-b.start()
 while True:
-    pass
+    print(us_sensor.distance_centimeters)
+    sleep(0.1)
