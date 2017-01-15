4cfff75f5217f695ed93f675431c0ca432683d4f
Ulli Hochholdinger <ulrich.hochholdinger@netpioneer.de>
2017-01-15 12:27:32 +0100

    AAAAAAAh!

--- test.py
+++ test.py
@@ -11,13 +11,22 @@ class Head(threading.Thread):
         self.daemon = True
         self.__distance = 100000
         self.__us_sensor = ev3.UltrasonicSensor()
+        self.__head_motor = ev3.MediumMotor('outB')
+        self.__head_motor.reset()
         assert self.__us_sensor.connected
+
+    def __bite(self):
+        self.__head_motor.polarity = 'inversed'
+        self.__head_motor.run_to_abs_pos(position_sp=180, speed=900, stop_action='hold')
+        sleep(1)
+        self.__head_motor.polarity = 'inversed'
+        self.__head_motor.run_to_abs_pos(position_sp=0, speed=500, stop_action='coast')
 
     def run(self):
         while True:
             self.__distance = self.__us_sensor.distance_centimeters
-            if self.__distance <= 100:
-                print("AAAAAhhh {}".format(self.__distance))
+            if self.__distance <= 400:
+                self.__bite()
             sleep(0.01)
 
 head = Head()
