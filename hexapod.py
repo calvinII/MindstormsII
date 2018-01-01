1d246f4bd23c2371d238a1596ded518c9d402b4c
Ulli Hochholdinger <ulrich.hochholdinger@diva-e.com>
2018-01-01 17:21:41 +0100

    Erste Schritte

--- hexapod.py
+++ hexapod.py
@@ -5,9 +5,8 @@ import ev3dev.ev3 as ev3
 motorA = ev3.LargeMotor(ev3.OUTPUT_A)
 motorD = ev3.LargeMotor(ev3.OUTPUT_D)
 
-motorA.reset()
-motorD.reset()
-motorA.run_to_rel_pos(position_sp=1080, speed_sp=900, stop_action="hold")
+
+otorA.run_to_rel_pos(position_sp=1080, speed_sp=900, stop_action="hold")
 motorD.run_to_rel_pos(position_sp=-1080, speed_sp=900, stop_action="hold")
 
 sleep(10)
